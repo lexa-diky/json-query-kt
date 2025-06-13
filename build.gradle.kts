408eb134dc9b9561d1ea5a4b7f0cd0878d8da20d
a.yakovlev <a.yakovlev@fhl.world>
2025-06-13 18:00:59 +0400

    bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import com.vanniktech.maven.publish.SonatypeHost
 import io.gitlab.arturbosch.detekt.Detekt
 
 plugins {
-    kotlin("multiplatform") version "2.1.20"
+    kotlin("multiplatform") version "2.1.21"
     kotlin("plugin.serialization") version "2.1.21"
-    kotlin("plugin.allopen") version "2.0.20"
+    kotlin("plugin.allopen") version "2.0.21"
 
     id("com.vanniktech.maven.publish") version "0.32.0"
     id("io.gitlab.arturbosch.detekt") version ("1.23.8")
